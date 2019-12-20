7c2f1969f4ce577a78b2648f9ccd44d5ec9b57e8
sajad_sadra <45831343+sajad-sadra@users.noreply.github.com>
2019-12-20 16:33:08 +0330

    Update yalda.sh

--- yalda.sh
+++ yalda.sh
@@ -4,4 +4,6 @@ docker stop yaldabot
 #2
 docker run --name yaldabot -p 8080:80 -e APACHE_LOG_DIR=/etc/apache2 -d sajadsadra/yaldabot 
 #3
-## open http://IP:8080/yalda in browser and click on .php file
+echo "-----------------------------------DONE-------------------"
+echo "open http://IP:8080/yalda in browser"
+echo "click on .php file"
